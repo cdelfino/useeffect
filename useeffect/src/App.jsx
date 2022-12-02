20f68f18611136502aa78ec6915c06d3f12b8d2f
Constanza Delfino <conidelfino@gmail.com>
2022-12-02 10:34:07 -0300

    rick api

--- useeffect/src/App.jsx
+++ useeffect/src/App.jsx
@@ -1,32 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [personajes, setPersonajes] =useState([])
+
+  //en apis se usa useeffect
+  // useEffect(parametro1, dependencia)
+  //DEPENDENCIA
+  // cuando no tiene nada ==> que se ejecuta con cada renderizado
+  // cuando tiene [] se ejecuta una vez
+  //[variables] cada vez que se cambie el valor de la variable, se va a ejecutar
+  console.log('se ejecuta')
+
+  useEffect(()=>{
+      console.log('se ejecuta useeffect')
+      const obtenerPersonajes = async()=>{
+        const response = await axios.get("https://rickandmortyapi.com/api/character")
+        setPersonajes(response.data.results)
+      }
+      obtenerPersonajes()
+
+  },[])
+  console.log('se termina de ejecutar useeffect')
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {personajes.map(elemento=>
+      <div className='card'>
+      <h2>{elemento.name}</h2>
+      <img src={elemento.image} alt="" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      )}
+
     </div>
   )
 }
